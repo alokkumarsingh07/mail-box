6bd6991a6c340082996b132ec90ae6474ff64cf8
alokkumarsingh07 <kalok8841@gmail.com>
2024-05-29 15:39:14 +0530

    API

---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+import authSlice from "./authSlice";
+import mailSlice from "./mailSlice";
+
+const store = configureStore({
+  reducer: {
+    auth: authSlice,
+    mail: mailSlice,
+  },
+});
+
+export default store;
